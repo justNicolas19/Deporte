ae52aab75003d86d91b4dbd6af6a77a795d367c8
nicolasf <nicolasestebanflorez1357@gmail.com>
2026-08-19 15:38:53 -0500

    Corredor

--- src/Corredor.java
+++ src/Corredor.java
@@ -2,8 +2,52 @@ public class Corredor extends Deportista {
     private double distancia;
     private double tiempo;
 
+    public Corredor(){
+
+    }
     public Corredor(double distancia, double tiempo) {
         this.distancia = distancia;
         this.tiempo = tiempo;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    @Override
+    public String toString() {
+        return "Corredor{" +
+                "distancia=" + distancia +
+                ", tiempo=" + tiempo +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", pais='" + pais + '\'' +
+                ", horasDeRendimiento=" + horasDeRendimiento +
+                '}';
+    }
+    public double calcularVelocidad(){
+        return this.distancia/this.tiempo;
+    }
+
+    @Override
+    public double calcularRendimiento(){
+        double velocidad = calcularVelocidad();
+        return velocidad * horasDeRendimiento;
+    }
+    public void mostrarDetalles(){
+        System.out.println("Distancia" + distancia);
+        System.out.println("Tiempo" + tiempo);
     }
 }
